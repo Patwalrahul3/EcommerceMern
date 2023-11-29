2fb9584f4c91a047cfaa38fa93ed4a1cb9f359a3
Rahul Patwal <patwalrahul3@gmail.com>
2023-11-29 23:14:39 +0530

    fetching using redux toolkit query

--- frontend/src/screens/HomeScreen.jsx
+++ frontend/src/screens/HomeScreen.jsx
@@ -1,34 +1,43 @@
 import { Row, Col } from "react-bootstrap";
-import axios from 'axios';
 import Product from "../components/Product";
-import { useState , useEffect } from "react";
+import { useGetProductsQuery } from "../slices/productsApiSlice";
+// import { useState , useEffect } from "react";
+// import axios from 'axios';
 
 const HomeScreen = () => {
 
-  const [products, setProducts] = useState([]);
+  const {data: products, isLoading, error} = useGetProductsQuery();
 
-  useEffect(()=>{
-   const fetchProducts = async() =>{
-    const {data} = await axios.get('/api/products');
-    console.log("Daa",data)
-    setProducts(data);
-   };
+  // const [products, setProducts] = useState([]);
 
-  fetchProducts();
+  // useEffect(()=>{
+  //  const fetchProducts = async() =>{
+  //   const {data} = await axios.get('/api/products');
+  //   console.log("Daa",data)
+  //   setProducts(data);
+  //  };
 
-  },[])
+  // fetchProducts();
+
+  // },[])
 
   return (
     <>
-    <h1>Latest Products</h1>
-    <Row>
-        {
-            products.map((product) =>( 
-              <Col key={product._id} sm={12} md={6} lg={4} xl={3}>
-                <Product product={product}  />
-              </Col>
-            ))}
-    </Row>
+
+    { isLoading ? (
+      <h2>Loading .........</h2>
+    ) : error ? (<div>{error?.data?.message || error.error}</div>) : (<>
+       <h1>Latest Products</h1>
+       <Row>
+           {
+               products.map((product) =>( 
+                 <Col key={product._id} sm={12} md={6} lg={4} xl={3}>
+                   <Product product={product}  />
+                 </Col>
+               ))}
+       </Row>
+    </>)}
+ 
     </>
   )
 }
